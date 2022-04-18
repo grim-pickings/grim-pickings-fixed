66c0db4eab34894394845f1a6be7a508811a92b7
connorjbryant <connorjamesbryant7@gmail.com>
2022-04-18 16:06:42 -0400

    Added my scenes with fire @ opponent stuff and end turn capability
    Warning: I had to update TextMeshPro from 3.0.6 to 3.0.5 for Unity and I'm using Unity version 2020.3.4f1

---
+++ GrimPickings/Assets/Scripts/FireScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireScript : MonoBehaviour
+{
+    //Use buttons to fire underneath the player (UI) and use SetActive to let players fire at the right times (for the prototype)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayerFire()
+    {
+        Debug.Log("Player fire");
+    }
+}
